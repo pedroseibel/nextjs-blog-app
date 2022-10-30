185c842071ea54d9bb8946de6a3d9f5d9d1e91f0
Pedro Seibel <pedrosbl@icloud.com>
2022-10-30 20:31:48 -0300

    Initial header layout and main banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
  import { AppProps } from 'next/app'
+ import '../styles/globals.css'
+ import 'tailwindcss/tailwind.css'
  
  function MyApp({Component, pageProps}) {
     return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,66 @@
 import Head from 'next/head'
+import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
 
-export default function Home() {
+interface Props {
+  posts: Post[]
+}
+
+export default function Home({ posts }: Props) {
+  console.log(posts)
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Next App</title>
         <link rel="icon" href="favicon.ico" />
       </Head>
+
+      <Header />
+
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-4 pr-2">
+              Lorem
+            </span>
+            ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+            impedit nemo corporis esse commodi nostrum omnis quisquam quo.
+          </h2>
+        </div>
+
+        <img
+          className="hidden md:inline-flex h-32 lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+        />
+      </div>
+
+      {/* Posts */}
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"] {
+    _id,
+    title,
+    author -> {
+    name,
+    image
+  },
+  description,
+  mainImage,
+  slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
